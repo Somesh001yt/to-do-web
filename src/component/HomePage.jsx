0bb7bbadd5d4bea5e96dceeb4b94d5208601e337
Shubham <someshcr7b@gmail.com>
2023-04-12 22:54:56 +0530

    new changes

--- src/component/HomePage.jsx
+++ src/component/HomePage.jsx
@@ -8,144 +8,199 @@ import {
   useColorModeValue,
   Td,
   Button,
-  Input
+  Input,
 } from "@chakra-ui/react";
 import { ExclamationCircleOutlined } from "@ant-design/icons";
 
 import Modal from "antd/es/modal/Modal";
+import { css } from "@emotion/react";
+import styled from "@emotion/styled";
 import { Pagination } from "antd";
-/* import Paginations from "./Paginations"; */
 
-const HomePage = () => {
-      const textColor = useColorModeValue("gray.700", "white");
-      const data = JSON.parse(localStorage.getItem("entries")) || [];
-      
-      const [formData, setFormData] = useState([]);
-      const [edit,setEdit]=useState(false)
-      const [editText,setEditText]=useState(data)
-    
-      useEffect(() => {
-        
-        setFormData(data);
-      }, []);
-    
-      const handleDelete = (index) => {
-        const newData = formData.filter((item, i) => i !== index);
-        localStorage.setItem("entries", JSON.stringify(newData));
-        setFormData(newData);
-      };
-      // const [visible, setVisible] = useState(false);
-    
-      const [isModalOpen, setIsModalOpen] = useState(false);
-      const Edit = (id) => {
-        setIsModalOpen(true);
-      console.log(id)
-      // const updatedId = formData.map((item, i) => i===id?setEditText(
-        
-      // ));
-      
-      };
-      const handleOk = () => {
-        setIsModalOpen(false);
-      };
-      const handleCancel = () => {
-        setIsModalOpen(false);
-      };
-      const [tableData, setTableData] = useState(() => {
-        const storedData = JSON.parse(localStorage.getItem("myTableData"));
-        return storedData || [];
-      });
-    
-      return (
-        <Table w="80%" m="auto" variant="simple" color={textColor}>
-          <Thead>
-            <Tr my=".8rem" pl="0px" color="black" bgColor="blue.400">
-              <Th pl="0px" color="black">
-                Title
-              </Th>
-              <Th color="black">Description</Th>
-              <Th color="black">Due Date</Th>
-              <Th color="black">Tag</Th>
-              <Th color="black">Status</Th>
-              <Th color="black">Timestamp</Th>
-              <Th color="black">Modify</Th>
-              <Th color="black">Delete</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
-            {formData.map((item, index) => (
-              <Tr key={index}>
-                <Td>{item.title}</Td>
-                <Td>{item.description}</Td>
-                <Td>{item.dueDate}</Td>
-                <Td>{item.tags}</Td>
-                <Td>{item.status}</Td>
-                <Td>{item.timestamp}</Td>
-                <Td>
-                  <Button type="primary" onClick={()=>{
-                           Edit(index)
-                  }}>
-                    Edit
-                  </Button>
-                  <Modal
-                    title="Basic Details"
-                    open={isModalOpen}
-                    onOk={handleOk}
-                    onCancel={handleCancel}
-                  >
-                    <Input
-                      value={isModalOpen?.title}
-                      onChange={(e) => {
-                        setIsModalOpen((pre) => {
-                          return { ...pre, title: e.target.value };
-                        });
-                        console.log(setIsModalOpen, "setISModalOpen");
-                      }}
-                    />
-                    <Input
-                      value={isModalOpen?.description}
-                      onChange={(e) => {
-                        setIsModalOpen((pre) => {
-                          return { ...pre, description: e.target.value };
-                        });
-                      }}
-                    />
-                    <Input
-                      value={isModalOpen?.dueDate}
-                      onChange={(e) => {
-                        setIsModalOpen((pre) => {
-                          return { ...pre, dueDate: e.target.value };
-                        });
-                      }}
-                    />
-                    <Input
-                      value={isModalOpen?.tags}
-                      onChange={(e) => {
-                        setIsModalOpen((pre) => {
-                          return { ...pre, tags: e.target.value };
-                        });
-                      }}
-                    />
-                    <Input
-                      value={isModalOpen?.status}
-                      onChange={(e) => {
-                        setIsModalOpen((pre) => {
-                          return { ...pre, status: e.target.value };
-                        });
-                      }}
-                    />
-                  </Modal>
-                </Td>
-                <Td>
-                  <Button colorScheme="red" onClick={() => handleDelete(index)}>
-                    Delete
-                  </Button>
-                </Td>
-              </Tr>
-            ))}
-          </Tbody>
-        </Table>
-      );
-}
+const StyledPagination = styled(Pagination)`
+  display: flex;
+  justify-content: center;
+  margin: 30px auto;
 
-export default HomePage
+  .ant-pagination-item-active {
+    background-color: #3182ce;
+    border-color: #3182ce;
+
+    a {
+      color: white;
+    }
+  }
+
+  .ant-pagination-item-link {
+    color: #3182ce;
+  }
+
+  .ant-pagination-next,
+  .ant-pagination-prev {
+    color: #3182ce;
+  }
+
+  .ant-pagination-prev .ant-pagination-item-link,
+  .ant-pagination-next .ant-pagination-item-link {
+    border-color: #3182ce;
+  }
+`;
+
+const HomePage = (page) => {
+  const textColor = useColorModeValue("gray.700", "white");
+  const data = JSON.parse(localStorage.getItem("entries")) || [];
+
+  const [formData, setFormData] = useState([]);
+  const [edit, setEdit] = useState(false);
+  const [editText, setEditText] = useState(data);
+  const itemsPerPage = 3;
+  const [currentPage, setCurrentPage] = useState(1);
+  const totalPages = Math.ceil(formData.length / itemsPerPage);
+
+  const startIndex = (currentPage - 1) * itemsPerPage;
+  const endIndex = startIndex + itemsPerPage;
+  const itemsToDisplay = formData.slice(startIndex, endIndex);
+
+  const todosPerPage = 5;
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  useEffect(() => {
+    setFormData(data);
+  }, []);
+
+  const handleDelete = (index) => {
+    const newData = formData.filter((item, i) => i !== index);
+    localStorage.setItem("entries", JSON.stringify(newData));
+    setFormData(newData);
+  };
+  // const [visible, setVisible] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const Edit = (id) => {
+    setIsModalOpen(true);
+    console.log(id);
+    // const updatedId = formData.map((item, i) => i===id?setEditText(
+
+    // ));
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
+  const [tableData, setTableData] = useState(() => {
+    const storedData = JSON.parse(localStorage.getItem("myTableData"));
+    return storedData || [];
+  });
+
+  return (
+    <Table
+      w="80%"
+      m="auto"
+      marginTop={"30px"}
+      variant="simple"
+      color={textColor}
+    >
+      <Thead>
+        <Tr my=".8rem" pl="0px" color="black" bgColor="blue.400">
+          <Th pl="30px" color="white">
+            Title
+          </Th>
+          <Th color="white">Description</Th>
+          <Th color="white">Due Date</Th>
+          <Th color="white">Tag</Th>
+          <Th color="white">Status</Th>
+          <Th color="white">Timestamp</Th>
+          <Th color="white">Modify</Th>
+          <Th color="white">Delete</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        {formData.map((item, index) => (
+          <Tr key={index}>
+            <Td>{item.title}</Td>
+            <Td>{item.description}</Td>
+            <Td>{item.dueDate}</Td>
+            <Td>{item.tags}</Td>
+            <Td>{item.status}</Td>
+            <Td>{item.timestamp}</Td>
+            <Td>
+              <Button
+                type="primary"
+                onClick={() => {
+                  Edit(index);
+                }}
+              >
+                Edit
+              </Button>
+              <Modal
+                title="Basic Details"
+                open={isModalOpen}
+                onOk={handleOk}
+                onCancel={handleCancel}
+              >
+                <Input
+                  value={isModalOpen?.title}
+                  onChange={(e) => {
+                    setIsModalOpen((pre) => {
+                      return { ...pre, title: e.target.value };
+                    });
+                    console.log(setIsModalOpen, "setISModalOpen");
+                  }}
+                />
+                <Input
+                  value={isModalOpen?.description}
+                  onChange={(e) => {
+                    setIsModalOpen((pre) => {
+                      return { ...pre, description: e.target.value };
+                    });
+                  }}
+                />
+                <Input
+                  value={isModalOpen?.dueDate}
+                  onChange={(e) => {
+                    setIsModalOpen((pre) => {
+                      return { ...pre, dueDate: e.target.value };
+                    });
+                  }}
+                />
+                <Input
+                  value={isModalOpen?.tags}
+                  onChange={(e) => {
+                    setIsModalOpen((pre) => {
+                      return { ...pre, tags: e.target.value };
+                    });
+                  }}
+                />
+                <Input
+                  value={isModalOpen?.status}
+                  onChange={(e) => {
+                    setIsModalOpen((pre) => {
+                      return { ...pre, status: e.target.value };
+                    });
+                  }}
+                />
+              </Modal>
+            </Td>
+            <Td>
+              <Button colorScheme="red" onClick={() => handleDelete(index)}>
+                Delete
+              </Button>
+            </Td>
+          </Tr>
+        ))}
+        <StyledPagination
+          pageSize={itemsPerPage}
+          total={totalPages}
+          onChange={handlePageChange}
+        />
+      </Tbody>
+    </Table>
+  );
+};
+
+export default HomePage;
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,12 +1,35 @@
-import { Box } from "@chakra-ui/react";
+import { Box, Flex, Spacer, Link as ChakraLink } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <Box>
-      <Link to="/">Home</Link>
-      <Link to="/taskform">Dashboard</Link>
-    </Box>
+    <Flex
+      as="nav"
+      align="center"
+      justify="space-between"
+      wrap="wrap"
+      padding="1rem"
+      bg="blue.500"
+      color="white"
+    >
+      <Box>
+        <Link to="/">
+          <ChakraLink fontSize="lg" fontWeight="bold">
+            To Do App
+          </ChakraLink>
+        </Link>
+      </Box>
+      <Spacer />
+      <Box display={{ base: "none", md: "block" }}>
+        <Link to="/">
+          <ChakraLink mr={8}>Form</ChakraLink>
+        </Link>
+        <Link to="/taskform">
+          <ChakraLink mr={8}>Dashboard</ChakraLink>
+        </Link>
+      </Box>
+    </Flex>
   );
 };
+
 export default Navbar;
